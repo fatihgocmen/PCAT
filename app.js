33f58ee16c17a4aea5e26bcf2372ec71a7711cff
Fatih GÖÇMEN <fatih.gocmen@ecocold.com.tr>
2023-03-14 11:16:31 +0300

    Workspaces updated

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-    console.log('fuction worked');
-};
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+const photo = {
+    id: 1,
+    name: 'Photo Name',
+    description: 'Photo Description'
+}
+
+app.get('/', (req, res) => {
+    res.send(photo)
+})
+
+app.listen(port, () => {
+    console.log(`Sunucu : ${port} başladı!`)
+})
