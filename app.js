260a00eeacc8064357c2c5482e0ee6ea4c7e2ebc
Fatih GÖÇMEN <fatih.gocmen@ecocold.com.tr>
2023-03-15 09:42:36 +0300

    Project routes and template engine added

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const ejs = require('ejs')
 const path = require('path')
 const port = 3000
 const app = express()
 
+app.set("view engine", "ejs")
+
+//Middlewares
 app.use(express.static('public'))
 
-
+//Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+    res.render('index')
+})
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/add', (req, res) => {
+    res.render('add')
 })
 
 app.listen(port, () => {
