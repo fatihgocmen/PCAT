e1c64f0dba29642613a106f4b3d3dacff971bbbd
Fatih GÖÇMEN <fatih.gocmen@ecocold.com.tr>
2023-03-15 17:09:11 +0300

    Project mongoose test page added

--- test.js
+++ test.js
@@ -1,20 +1,17 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-mongoose.set('strictQuery', false);
-mongoose.set('autoIndex', false);
 mongoose.connect("mongodb://127.0.0.1:27017/pcat-test-db");
 
-const photoScheme = new Schema({
+const PhotoScheme = new Schema({
     title: String,
     description: String,
 });
-photoScheme.set('autoIndex', false);
 
-const photo = mongoose.model('Photo', photoScheme);
+const Photo = mongoose.model('Photo', PhotoScheme);
 
 
-photo.create({
+Photo.create({
     title: 'Photo Title 1',
     description: 'Photo 1 description 1 lorem ipsum'
 });
